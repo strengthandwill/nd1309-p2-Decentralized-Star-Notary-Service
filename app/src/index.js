59a245125ad917d9d11e1de4bdcd26bced6f5844
poh_kah_kong <poh_kah_kong@tech.gov.sg>
2021-08-29 20:18:59 +0800

    Update lookUp

--- app/src/index.js
+++ app/src/index.js
@@ -34,16 +34,22 @@ const App = {
   createStar: async function() {
     const { createStar } = this.meta.methods;
     const name = document.getElementById("starName").value;
+    const symbol = document.getElementById("starSymbol").value;
     const id = document.getElementById("starId").value;
-    await createStar(name, id).send({from: this.account});
+    await createStar(name, symbol, id).send({from: this.account});
     App.setStatus("New Star Owner is " + this.account + ".");
   },
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    
+    const { tokenIdToStarInfo } = this.meta.methods;    
+    const id = document.getElementById("lookid").value;
+    const star = await tokenIdToStarInfo(id).call({from: this.account});  
+    App.setStatus(
+      star.name.length == 0?
+      "Sorry, the star does not exists." :
+      "The star name is " + star.name + " with symbol of " + star.symbol + ".");
   }
-
 };
 
 window.App = App;
